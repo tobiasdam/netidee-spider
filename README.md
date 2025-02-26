6b9f1513235f6413b4c3019cca0de776ab9d42ba
tobiasdam <20456596+tobiasdam@users.noreply.github.com>
2025-02-26 22:47:17 +0100

    make use of scrapy options

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # netidee project crawler
 
 This repository is part of the [CrOSSD 2](https://www.netidee.at/crossd2) project. It uses a [scrapy](https://scrapy.org/) spider to collect all github.com urls from the [netidee](https://www.netidee.at/) website.
-In case of Github user/organization urls, all repositories of that user/organization collected instead.
+In case of Github user/organization urls, all repositories of that user/organization might be collected instead.
 
 ## Usage
 
@@ -13,13 +13,42 @@ pipenv install
 Run spider either with:
 ```bash
 pipenv run main
+# stores data in data_strict.csv
+# uses strict mode
 ```
 or
 ```bash
-scrapy runspider netidee.py
+scrapy runspider netidee.py -O <filename.format> [-a resolve_users=False|True] [-a exclude_users=False|True] [-a mode=strict|relaxed]
 ```
 
-The results are saved in `github_links.txt`.
+### Output formats
+
+`-O` stores the crawled URLs in the specified filename in the specified format (`name.format`), overwriting previous content.
+
+`-o` behaves the same, besides appending to existing files.
+
+Scrapy supports following output formats:
+
+- json
+- jsonlines
+- jsonl
+- jl
+- csv
+- xml
+- marshal
+- pickle
+
+## Parameters
+
+Parameters are added via the scrapy option `-a key=vale`.
+
+Following parameters are supported:
+
+|Parameter|Default|Values|Description|
+|---|---|---|---|
+|resolve_users|False|Boolean|Collects alles repositories of user/orga (`https://github.com/username`)|
+|exclude_users|False|Boolean|Ignore github urls only containing user/orga (no repository)|
+|mode|"strict"|"strict" \| "relaxed"|Ignore URLs that contain a remainder after the repository|
 
 ## Acknowledgements
 
--- netidee.py
+++ netidee.py
@@ -3,24 +3,46 @@ import urllib.request
 import re
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+from enum import StrEnum
+
+
+class Mode(StrEnum):
+    relaxed = "relaxed"
+    strict = "strict"
 
 
 class NetideeSpider(CrawlSpider):
     name = "netidee"
     allowed_domains = ["www.netidee.at", "github.com"]
     start_urls = ["https://www.netidee.at/"]
-    regex = re.compile(r"https://github.com/([^/]+)/*([^/?#]*)")
-    gf = open("github_links.txt", "w")
-    result = set()
+    regex_dict = {
+        "relaxed": r"https://github.com/([^/]+)/*([^/?#]*)",
+        "strict": r"https://github.com/([^/]+)/*([^/?#]*)/?$",
+    }
+    # gf = open("github_links.txt", "w")
+
+    def __init__(self, resolve_users=False, exclude_users=False, mode=Mode.strict, *args, **kwargs):
+        super(NetideeSpider, self).__init__(*args, **kwargs)
+        self.result = set()
+        self.resolve_users = resolve_users in ("True", "true")
+        self.exclude_users = exclude_users in ("True", "true")
+
+        match mode.lower():
+            case Mode.relaxed | Mode.strict:
+                self.mode = Mode(mode)
+                self.regex = re.compile(self.regex_dict[self.mode])
+            case _:
+                print("unsupported mode")
+                exit(1)
 
     rules = (
         Rule(
-            LinkExtractor(allow=r".*", allow_domains=["www.netidee.at"]),
+            LinkExtractor(allow=r".*", allow_domains=["www.netidee.at"], unique=True),
             callback="parse_item",
             follow=True,
         ),
         Rule(
-            LinkExtractor(allow=r".*", allow_domains=["github.com"]),
+            LinkExtractor(allow=r".*", allow_domains=["github.com"], unique=True),
             callback="parse_github",
             follow=False,
         ),
@@ -30,18 +52,31 @@ class NetideeSpider(CrawlSpider):
         item = {}
         print(response.url)
         tmp = re.match(self.regex, response.url.strip())
+        if tmp.group().strip() in self.result:
+            return item
         if tmp.groups()[1]:
             # url with repo
             self.result.add(tmp.group().strip())
+            item["url"] = tmp.group().strip()
+
         else:
             # url of orgs or users
-            with urllib.request.urlopen(
-                f"https://api.github.com/users/{tmp.groups()[0]}/repos"
-            ) as response:
-                data = response.read()
-                json_data = json.loads(data)
-                self.result |= set(elem["html_url"] for elem in json_data)
-        item["url"] = response.url
+            if self.exclude_users:
+                return item
+
+            if self.resolve_users:
+                with urllib.request.urlopen(
+                    f"https://api.github.com/users/{tmp.groups()[0]}/repos"
+                ) as response:
+                    data = response.read()
+                    json_data = json.loads(data)
+                    # self.result |= set(elem["html_url"] for elem in json_data)
+                    item = [{"url": elem["html_url"]} for elem in json_data]
+            else:
+                # self.result.add(tmp.group().strip())
+                item["url"] = tmp.group().strip()
+
+        # item["url"] = response.url
         return item
 
     def parse_item(self, response):
@@ -49,7 +84,8 @@ class NetideeSpider(CrawlSpider):
         return item
 
     def closed(self, reason):
-        for elem in self.result:
-            self.gf.write(elem)
-            self.gf.write("\n")
-        self.gf.close()
+        # for elem in self.result:
+        #     self.gf.write(elem)
+        #     self.gf.write("\n")
+        # self.gf.close()
+        pass
